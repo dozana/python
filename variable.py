ae2fa18dce771c23e0d2c55df5f9f99ca1c01f98
Levan <levan.soroznishvili@gmail.com>
2024-01-02 23:53:58 +0400

    variable

---
+++ variable.py
@@ -0,0 +1,9 @@
+# Define a variable named 'age' and assign it a value
+age = 30
+
+# Define another variable named 'name' and assign it a string value
+name = "Alice"
+
+# Print the values of both variables
+print("Name:", name)
+print("Age:", age)
