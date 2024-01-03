ca4de311a85fc014e2ef41add767c33bac6ea2e5
Levan <levan.soroznishvili@gmail.com>
2024-01-04 00:02:11 +0400

    Coding Exercise Data Types

---
+++ day-2/f3.py
@@ -0,0 +1,11 @@
+# [Interactive Coding Exercise] Data Types
+
+two_digit_number = input()
+
+# print(type(two_digit_number))
+
+num1 = int(two_digit_number[0])
+num2 = int(two_digit_number[1])
+res = num1 + num2
+
+print(res)
