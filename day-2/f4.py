65ced969f5724eada6cca9e5c07c9ad4c3e819eb
Levan <levan.soroznishvili@gmail.com>
2024-01-04 00:14:08 +0400

    Mathematical Operations in Python

---
+++ day-2/f4.py
@@ -0,0 +1,23 @@
+# Mathematical Operations in Python
+
+'''
+PEMDAS
+
+Parentheses     ()
+Exponents       **
+
+Multiplication  *
+Division        / 
+
+Addition        +
+Subtraction     -
+'''
+
+print(3 + 5)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3)
+print(2 ** 3)
+
+print((3 * 3) + (3 / 3) - 3)
+print(9 + (3 / 3) - 3 - 3)
