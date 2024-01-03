064dc0893b5cca3c966c6d0dd48c1250fb8e50da
Levan <levan.soroznishvili@gmail.com>
2024-01-03 23:35:51 +0400

    data types

---
+++ day-2/f1.py
@@ -0,0 +1,23 @@
+# Data types
+
+
+# String
+print("--- String ---")
+print("Hello"[0])
+print("123" + "345")
+print("hello" + "345")
+
+
+# Integer
+print("--- Integer ---")
+print(123 + 345)
+print(123_456_789)
+
+
+# Float
+print("--- Float ---")
+print(3.14159)
+
+
+# Boolean
+print("--- Boolean ---")
