fa5fab49b7f680723518dd96ee601166575b52fa
Levan <levan.soroznishvili@gmail.com>
2024-01-04 13:39:40 +0400

    find odd or even number

---
+++ day-3/f2.py
@@ -0,0 +1,8 @@
+# [Interactive Coding Exercise] Odd or Even? Introducing the Modulo
+
+number = int(input("Enter number: "))
+
+if number % 2 == 0 :
+  print("This is an even number. With no remainder")
+else:
+  print(f"This is an odd number. Reminder is {number % 2}")
