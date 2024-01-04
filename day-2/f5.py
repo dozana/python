9b6b2b726adfa9b1f432031855566e17edbd1c02
Levan <levan.soroznishvili@gmail.com>
2024-01-04 07:37:15 +0400

    BMI calculator

---
+++ day-2/f5.py
@@ -0,0 +1,7 @@
+#  [Interactive Coding Exercise] BMI Calculator
+
+weight_kg = float(input("Enter your weight in kilograms: "))
+height_m = float(input("Enter your height in meters: "))
+bmi = weight_kg / height_m ** 2
+
+print(bmi)
