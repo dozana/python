33c9abce5967daaf0cd8fb17e91c970ae0f95b08
Levan <levan.soroznishvili@gmail.com>
2024-01-07 15:05:26 +0400

    the Offset and Appending Items to Lists v2

--- day-4/f4.py
+++ day-4/f4.py
@@ -2,8 +2,18 @@
 
 import random
 
-names_string = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
-names_length = len(names_string)
-random_index = random.randint(0, names_length - 1)
+# version 1
+# names_string = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+# names_length = len(names_string)
+# random_index = random.randint(0, names_length - 1)
 
-print(f"{names_string[random_index]} is going to buy the meal today!")
+# print(f"{names_string[random_index]} is going to buy the meal today!")
+
+
+# version 2
+names_string = input("Enter names: ")
+names = names_string.split(", ")
+num_items = len(names)
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice ]
+print(f"{person_who_will_pay} is going to buy the meal today!")
