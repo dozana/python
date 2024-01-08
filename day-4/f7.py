21123400e7f1d24a7e479c0ced3448ca7781e86b
Levan <levan.soroznishvili@gmail.com>
2024-01-08 09:14:07 +0400

    solved little bug

--- day-4/f7.py
+++ day-4/f7.py
@@ -1,4 +1,4 @@
-# 
+# Project: Rock Paper Scissors
 
 import random
 
@@ -29,7 +29,6 @@ scissors = '''
 ---.__(___)
 '''
 
-
 '''
 # The Basic Rules
 0 - Rock wins against scissors.
@@ -40,23 +39,21 @@ scissors = '''
 game_images = [rock, paper, scissors]
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
-computer_choice = random.randint(0, 2)
-
-print("\n")
-print(f"My chose: {game_images[user_choice]}")
-print(f"PC chose: {game_images[computer_choice]}")
 
 if user_choice >= 3 or user_choice < 0:
   print("You typed an invalid number, you lose!")
-elif user_choice == 0 and computer_choice == 2:
-  print("You win!")
-elif computer_choice == 0 and user_choice == 2:
-  print("You lose")
-elif computer_choice > user_choice:
-  print("You lose")
-elif user_choice > computer_choice:
-  print("You win!")
-elif computer_choice == user_choice:
-  print("It's a draw") 
+else:
+  print(f"\nMy chose: {game_images[user_choice]}")
+  computer_choice = random.randint(0, 2)
+  print(f"PC chose: {game_images[computer_choice]}")
   
-print("\n")
+  if user_choice == 0 and computer_choice == 2:
+    print("You win!")
+  elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+  elif computer_choice > user_choice:
+    print("You lose")
+  elif user_choice > computer_choice:
+    print("You win!")
+  elif computer_choice == user_choice:
+    print("It's a draw")
