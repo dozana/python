74189560bba73e9b3fa68dd52fd04116f755faf9
Levan <levan.soroznishvili@gmail.com>
2024-01-10 17:38:09 +0400

    Password Generator Project

---
+++ day-5/f8.py
@@ -0,0 +1,27 @@
+#Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+r_letters = random.sample(letters, nr_letters)
+r_symbols = random.sample(symbols, nr_symbols)
+r_numbers = random.sample(numbers, nr_numbers)
+
+easy_password = r_letters + r_symbols + r_numbers
+easy_password_str = ''.join(easy_password)
+print(f"Easy Password: {easy_password_str}")
+
+random.shuffle(easy_password)
+hard_password_str = ''.join(easy_password)
+
+print(f"Hard Password: {hard_password_str}")
