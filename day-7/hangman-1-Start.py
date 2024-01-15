8c5113b7db715bb42785fb8fe5d0b002ea5c40e7
Levan <levan.soroznishvili@gmail.com>
2024-01-15 23:21:51 +0400

    hangman part 1 completed

--- day-7/hangman-1-Start.py
+++ day-7/hangman-1-Start.py
@@ -2,13 +2,21 @@ import random
 
 #Step 1 
 
-word_list = ["aardvark", "baboon", "camel"]
+word_list = ["Aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# Step 1
+word_list = ["Aardvark", "baboon", "camel"]
 
-print(random.choice(word_list))
+# TODO-1: Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# TODO-2: Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# TODO-3: Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+if guess in chosen_word.lower():
+    print("Yes, it's in the word.")
+else:
+    print("No, it's not in the word.")
+
 
