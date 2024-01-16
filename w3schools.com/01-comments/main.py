295a4cf1774b42734c5f52a605ee5fce0cdb2c43
Levan <levan.soroznishvili@gmail.com>
2024-01-16 15:12:33 +0400

    multiple comments

--- w3schools.com/01-comments/main.py
+++ w3schools.com/01-comments/main.py
@@ -1,6 +1,21 @@
+# SINGLE LINE COMMENT
+
 # this is a comment
-print("hello World")
-
+print("hello World 1")
 print("Hi there") # this is a comment
-
 # print("another comment")
+
+
+# MULTI LINE COMMENT
+
+# this is a comment
+# written in
+# more than just one line
+print("Hello World 2")
+
+"""
+this is a comment
+ written in
+# more than just one line
+"""
+print("Hello world 3")
