dd0188cb3c291cfabf0e0cab201d333e44ffd353
Levan <levan.soroznishvili@gmail.com>
2024-01-24 11:56:45 +0400

    variable names

---
+++ w3schools.com/02-variables/02-variable-names.py
@@ -0,0 +1,23 @@
+# Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Illegal variable names:
+'''
+2myvar = "John"
+my-var = "John"
+my var = "John"
+'''
+
+# camel case
+myVariableName = "John"
+
+# pascal case
+MyVariableName = "John"
+
+# snake case
+my_variable_name = "John"
