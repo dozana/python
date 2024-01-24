4badcedfd781f7999993f7a2e358b3c4407da633
Levan <levan.soroznishvili@gmail.com>
2024-01-24 19:52:50 +0400

    strings

---
+++ w3schools.com/06-strings/01-string.py
@@ -0,0 +1,26 @@
+# Strings
+
+'''
+Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+'hello' is the same as "hello".
+
+You can display a string literal with the print() function:
+'''
+print("Hello")
+print('Hello')
+
+
+# Assign String to a Variable
+a = "Hello"
+print(a)
+
+print("------")
+
+# Multiline Strings
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
