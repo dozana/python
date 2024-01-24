c66d8f64221b40fa33803ff8d888b71d6f6b4fe9
Levan <levan.soroznishvili@gmail.com>
2024-01-24 18:40:24 +0400

    assign multiple values

---
+++ w3schools.com/02-variables/03-assign-multiple-values.py
@@ -0,0 +1,23 @@
+# Many Values to Multiple Variables
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+print("------")
+
+# One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+print("------")
+
+# Unpack a Collection
+fruits = ["Orange", "Banana", "Cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
