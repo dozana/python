e76a2393869f3d7c5bcbe401f7f570dbbc8a844e
Levan <levan.soroznishvili@gmail.com>
2024-01-24 18:44:44 +0400

    output variables

---
+++ w3schools.com/02-variables/04-output-variables.py
@@ -0,0 +1,26 @@
+# Output Variables
+x = "Python is awesome"
+print(x)
+
+print("------")
+
+# In the print() function, you output multiple variables, separated by a comma:
+# The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types:
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+print("------")
+
+# You can also use the + operator to output multiple variables:
+# Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+print("------")
+x = 5
+y = 10
+print(x + y)
