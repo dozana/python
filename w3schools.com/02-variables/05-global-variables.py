0c6bc2c99723b19fbff6dcd3077c54708c0e8659
Levan <levan.soroznishvili@gmail.com>
2024-01-24 19:48:17 +0400

    structure changed

--- w3schools.com/02-variables/05-global-variables.py
+++ w3schools.com/02-variables/05-global-variables.py
@@ -12,7 +12,9 @@ myfunc()
 print("------")
 
 '''
-If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function. The global variable with the same name will remain as it was, global and with the original value.
+If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function. 
+
+The global variable with the same name will remain as it was, global and with the original value.
 '''
 
 x = "awesome"
