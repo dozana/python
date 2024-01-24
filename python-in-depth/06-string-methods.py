ddae1837330da34102b9f173069fc5a7585de54f
Levan <levan.soroznishvili@gmail.com>
2024-01-24 11:23:43 +0400

    python 3 bootcamp

---
+++ python-in-depth/06-string-methods.py
@@ -0,0 +1,10 @@
+## Methods and functions we can use on String objects
+# Official documentation: https://docs.python.org/3/library
+# We ran the following functions on strings
+# len(), type(), id(), dir()
+# We ran the following string methods
+# capitalize(), upper(), lower(), strip(), find()
+# split(), join()
+# Test them out on the variable declaration below, two examples
+# provided
+
